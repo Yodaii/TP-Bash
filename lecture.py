8aae5e5dda7969257af16ee75ea9daaacf920c71
Yodaii <yohann.vaubourg@gmail.com>
2014-11-13 19:27:46 +0100

    2.2 + 3
    Python finit ou presque

---
+++ lecture.py
@@ -0,0 +1,46 @@
+'''
+Created on 13 nov. 2014
+
+@author: Sohan
+'''
+from pip._vendor.distlib.compat import raw_input
+
+fichier = open("passwd.txt", "r")
+iterationLigne = 0
+listLigne = [fichier.readline()]
+
+while 1:
+    listLigne.append(fichier.readline())
+    if listLigne[iterationLigne] == "":
+        break
+    iterationLigne = iterationLigne + 1
+    
+print(listLigne)
+fichier.close()
+
+nom = raw_input("Could you write the user name?")
+info = ""
+ 
+i = 0
+while i != iterationLigne:
+    if nom in listLigne[i]:
+        info = listLigne[i]
+        break
+    else:
+        i = i + 1
+        
+if info == "":
+    print ("Error: " + nom + " is not an user name valid.")
+else:
+    info = info.split(":")    
+    print("The information asked is the following:")
+    print("User Name: " + info[0])
+    print("User Identifier: " + info[2])
+    print("Group Identifier: " + info[3])
+    if info[4] == "":
+        print("Gecos Field: --")
+    else:
+        print("Gecos Field: " + info[4])
+    print("Personal Directory: " + info[5])
+    print("Shell: " + info[6])
+    
