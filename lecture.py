1db18a631d82d4d2f84da668cb01522a99a26607
Yodaii <yohann.vaubourg@gmail.com>
2014-11-27 14:07:14 +0100

    Commentaires
    commentaires du code

--- lecture.py
+++ lecture.py
@@ -5,34 +5,51 @@ Created on 13 nov. 2014
 '''
 from pip._vendor.distlib.compat import raw_input
 
+# variable contenant le fichier passwd
 fichier = open("passwd.txt", "r")
-iterationLigne = 0
-listLigne = [fichier.readline()]
 
+# Variable contenant toutes les lignes du fichier sous forme de liste
+listLigne = [fichier.readline()] # ici on affecte uniquement la premiere ligne
+
+# Variable permettant de se situer dans la liste
+iterationLigne = 0
+
+# Boucle While qui s'arrete quand on a parcouru toutes les lignes du fichier
 while 1:
-    listLigne.append(fichier.readline())
+    listLigne.append(fichier.readline()) # ici on ajoute une ligne du fichier supplementaire a la liste
+    # si la ligne ajouter est vide on sort de la boucle
     if listLigne[iterationLigne] == "":
         break
+    # on icremente pour svoir combien de ligne contient le fichier et ne pas prendre en compte la ligne vide que le vient peut etre d ajouter
     iterationLigne = iterationLigne + 1
     
-print(listLigne)
+# on ferme le fichier apres que la lecture soit fini
 fichier.close()
 
+# variable contenant le nom entree par l utilisateur
 nom = raw_input("Could you write the user name?")
+# variable initialiser a vide qui contiendra les infos du nom
 info = ""
- 
+# iteration i de la boucle while 
 i = 0
+
+# boucle while qui s'arrete lorsque l on a parcouru toutes la liste
 while i != iterationLigne:
-    if nom in listLigne[i]:
-        info = listLigne[i]
+    # si le nom rentrer correspond a la premiere valeur de la chaine contenu a l emplacement i de la liste 
+    # alors on recupere les informations du nom teste et on sort de la boucle
+    if nom in listLigne[i].split(":")[0]:
+        info = listLigne[i].split(":")
         break
+    # sinon on incremente i et on recommence la boucle
     else:
         i = i + 1
-        
+ 
+# si la variable info est toujours vide ca signifie que le nom n est pas dans le fichier
+# alors on le specifie q l utilisateur       
 if info == "":
     print ("Error: " + nom + " is not an user name valid.")
+# sinon on affiche les informations
 else:
-    info = info.split(":")    
     print("The information asked is the following:")
     print("User Name: " + info[0])
     print("User Identifier: " + info[2])
--- pass.py
+++ pass.py
@@ -6,31 +6,39 @@ Created on 13 nov. 2014
 from pip._vendor.distlib.compat import raw_input
 import re
 
-
+# Condition de la boucle while
 continuer = 1
 
+# Boucle While permettant de recommencer l operation plusieurs fois tant que continuer vaut 1
 while continuer == 1:
+    # Valeur permettant d'indiquer que le pass est correct (condition d'arret)
     passBon = 0
     
+    # Boucle While permettant de restester un mot de pass tant qu'un n'est pas correct
     while passBon == 0:
-        mdp = raw_input("Quel est votre mot de passe?")
-        aNbCar = len(mdp)
-        aNombre = r"[0-9]"
+        mdp = raw_input("Quel est votre mot de passe?") # variable contenant le mot de passe
+        aNbCar = len(mdp) # variable contenant la longueure du mot de passe
+        aNombre = r"[0-9]" 
         aCarSpec = r"[^0-9a-zA-Z]"
     
+        # Test si le mot de passe contient au moins 8 caracteres
         if aNbCar < 8:
             print("Saisissez un mot de passe contenant au moins 8 caracteres.")
             aNbCar = len(mdp)
-            continue
+            continue #permet de retourner au debut de la boucle
     
+        # Test si le mot de passe contient au moins 1 chiffre
         if re.search(aNombre, mdp) is None:
             print("Saisissez un mot de passe avec un chiffre.")
-            continue
+            continue #permet de retourner au debut de la boucle
     
+        # Test si le mot de pass contient au moins 1 caractre speciale
         if re.search(aCarSpec, mdp) is None:
             print("Saisissez un mot de passe avec un caractere speciale.")
-            continue
+            continue #permet de retourner au debut de la boucle
         
+        # si tout les tests sont verifier, le mot de passe est correct, on sort de la boucle
         passBon = 1
         
+    #permet de savoir si l'utilisateur veux recommencer
     continuer = int(input("Voulez-vous essayer un autre mot de passe? (1= Oui / 0= Non)"))
